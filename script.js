e147584b9300dce57a5e2a1da3c218f1ac79d13c
mervezgin <mervezgin@gmail.com>
2023-01-31 10:07:19 +0300

    updates

--- script.js
+++ script.js
@@ -29,9 +29,19 @@ function update() {
             circle.classList.add('active')
         } else {
             circle.classList.remove('active')
+        }
     })
 
     const actives = document.querySelectorAll('.active')
 
-    console.log(actives.length, circle.length)
+    progress.style.width = (actives.length - 1) / (circle.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true 
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
 }
